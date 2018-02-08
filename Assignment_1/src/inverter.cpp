7c7cdda88092b50e48b854b18ed6d826a68252b5
tnlong1997 <tnlong1997@gmail.com>
2018-02-07 22:50:53 -0500

    Assignment 1

---
+++ Assignment_1/src/inverter.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <queue>
+#include <map>
+#include <set>
+
+#include "inverter.h"
+
+using namespace std;
+
+
+string build_inverted_index(string filename){
+	return "";
+}
